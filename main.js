34fe608bb0189f8f80c9d65fe0791a9dced127c9
Abigail Powell <powheart26@proton.me>
2025-01-30 01:57:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ animate()
 function init() {
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
   camera.position.x = 2
+  scene.background = new THREE.Color( 0x1b0d72 );
     
   scene = new THREE.Scene();
     
   geometry = new THREE.SphereGeometry(1);
-  material = new THREE.MeshBasicMaterial({ color: 0x1b0d72, wireframe: true, wireframeLinewidth: 2});
+  material = new THREE.MeshBasicMaterial({ color: 0xfcce30, wireframe: true, wireframeLinewidth: 2});
   mesh = new THREE.Mesh(geometry, material);
   
   scene.add(mesh);
@@ -26,8 +27,8 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
 
-  mesh.rotation.x = Date.now() * 0.00009;
-  mesh.rotation.y = Date.now() * 0.00009;
+  mesh.rotation.x = time * 0.00009;
+  mesh.rotation.y = time * 0.00009;
 
   renderer.render(scene, camera);
 }
